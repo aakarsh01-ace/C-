70a48e2142290f2a77681b71843513b194ea1de3
Aakarsh Chaurasia <110169644+aakarsh01-ace@users.noreply.github.com>
2024-07-10 06:55:47 +0000

    Day 1

--- Learn_basic_recursion.cpp/basic_recurrsion_problems.cpp
+++ Learn_basic_recursion.cpp/basic_recurrsion_problems.cpp
@@ -37,21 +37,55 @@
 // }
 
 //Question 3: print from n to 1 using recursion
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void f(int i, int n){
+//   if(i<=0) return;
+//   cout<<i<<" ";
+//   f(i-1, n);
+// }
+
+// int main(){
+//   int a;
+//   cin>>a;
+//   f(a,a); // we tool a,a instead of simply i,a because a will behave like i and will eventually be decreased at every recursive step;
+//   return 0;
+// }
+
+//Question 4: sum of first n numbers using recurrsion
+// Method 1: Parametereised way
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void f(int i, int sum){
+//   if(i<1){
+//     cout<<sum;
+//     return;
+//   }
+//   f(i-1, sum+i);
+// }
+
+// int main(){
+//   int a;
+//   cin>>a;
+//   f(a, 0);
+//   return 0;
+// }
+
+// Method 2: Functional way
 #include <bits/stdc++.h>
 using namespace std;
 
-void f(int i, int n){
-  // similar to the first problem, here we will check if i is less than 1 or not
-  if(i<1) return; // program gets terminated
-  // if not print the value of i
-  cout<<i<<" ";
-  f(i-1, n);
-  
+int f(int n){
+  if(n==0) return 0;
+  return n+f(n-1);
 }
 
 int main(){
-  int n;
-  cin>>n;
-  f(n,n);
+  int a;
+  cin>>a;
+  int sum = f(a);
+  cout<<sum;
   return 0;
 }
