5052bbd0e571927d0a80bff67905b24d9a22e65a
Aakarsh Chaurasia <110169644+aakarsh01-ace@users.noreply.github.com>
2024-09-06 18:05:32 +0000

    Update commit

---
+++ .ccls-cache/@home@runner@C/AC_PRACT@ques_41_leetcode.cpp
@@ -0,0 +1,28 @@
+// cpp code to check if the person is older than 60 years old or not
+#include <bits/stdc++.h>
+using namespace std;
+
+int CountOfPerson(vector<string> details){
+  int n = details.size();
+  int count = 0;
+
+  for(int i = 0; i < n; i++){
+    int age = stoi(details[i].substr(11, 2));
+
+    if(age>60){
+      count++;
+    }
+  }
+  return count;
+}
+
+int main(){
+  vector<string> details(15);
+  for(int i = 0; i<15; i++){
+    cin>>details[i];
+  }
+
+  int result = CountOfPerson(details);
+  cout<<result;
+  return 0;
+}
---
+++ AC_PRACT/ques_40.cpp
@@ -0,0 +1,47 @@
+// return index value if the desired element is present in the array
+#include <bits/stdc++.h>
+using namespace std;
+
+int IndexVal(vector<int> arr, int n, int divisor, int quotient, int remainder){
+  int dividend = divisor * quotient + remainder;
+  sort(arr.begin(), arr.end());
+  int left = 0;
+  int right = n-1;
+
+  for(int i = 0; i < n; i++){
+    if(arr[i] == dividend){
+      return i;
+      break;
+    }else if(arr[i] < dividend){
+      left++;
+    }else if(arr[i] > dividend){
+      right--;
+    }else{
+      return -1;
+    }
+  }
+  return -1;
+}
+
+int main(){
+  int n;
+  cin>>n;
+
+  vector<int> arr(n);
+  for(int i = 0; i<n; i++){
+    cin>>arr[i];
+  }
+
+  int divisor;
+  cin>>divisor;
+
+  int quotient;
+  cin>>quotient;
+
+  int remainder;
+  cin>>remainder;
+
+  int result = IndexVal(arr, n, divisor, quotient, remainder);
+  cout<<result;
+  return 0;
+}
